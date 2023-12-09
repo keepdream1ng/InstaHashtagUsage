a9d82dffc329d1c0a49b64a166f44285e3e9abb9
Anatolii Ivanov <gotnickname@gmail.com>
2023-12-09 15:10:55 +0530

    chars for input cleanup are read from config

--- InstaHashtagUsage.ClassLibrary/Mediatr/NewInput/NewInputHandler.cs
+++ InstaHashtagUsage.ClassLibrary/Mediatr/NewInput/NewInputHandler.cs
@@ -6,16 +6,19 @@ public class NewInputHandler : INotificationHandler<NewInputNotification>
 {
 	private readonly ILogger<NewInputHandler> _logger;
 	private readonly IHashtagQueue _hashtagQueue;
+	private readonly IConfiguration _configuration;
 	private readonly IMediator _mediatr;
 
 	public NewInputHandler(
         ILogger<NewInputHandler> logger,
 		IHashtagQueue hashtagQueue,
+		IConfiguration configuration,
 		IMediator mediatr
         )
     {
 		_logger = logger;
 		_hashtagQueue = hashtagQueue;
+		_configuration = configuration;
 		_mediatr = mediatr;
 	}
 
@@ -23,7 +26,7 @@ public class NewInputHandler : INotificationHandler<NewInputNotification>
 	{
 		string[] hashtags = notification.NewInput
 			.ToLower()
-			.Split(new char[] { ' ', '.', ',', '#' }, StringSplitOptions.RemoveEmptyEntries);
+			.Split(_configuration.GetSection("cleanThisChars").Get<char[]>(), StringSplitOptions.RemoveEmptyEntries);
 		_logger.LogInformation("Input splitted into array {hashtags}", (object)hashtags);
 		await _hashtagQueue.AddRangeAsync(hashtags);
 		_mediatr.Publish(new ProcessHashtagNotification());
